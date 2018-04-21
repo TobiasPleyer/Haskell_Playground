45e0ef470524be8821914b34422248800d84f2f3
TobiasPleyer <tobi.pleyer@gmail.com>
2018-04-21 15:18:57 +0200

    Improve the toy server example

--- Haskell/server.hs
+++ Haskell/server.hs
@@ -2,72 +2,119 @@
 {- stack
   script --resolver lts-9.12
   --package base
+  --package containers
   --package transformers
 -}
 {-# LANGUAGE PatternGuards #-}
 
 import Control.Monad
+import Control.Monad.IO.Class
 import Control.Monad.Trans.State.Strict
+import qualified Data.Map.Strict as M
 import Data.Maybe
 
 
-data Request = Request { requestType :: String, requestBody :: String }
-type Handler = (Request -> Maybe String)
-newtype ServerState = ServerState [Handler]
-type Server = State ServerState ()
+type Body = String
+type Pattern = String
+data Request = Request { requestType :: Pattern, requestBody :: Body }
+type Handler = (Pattern -> Maybe Action)
+data ServerData = SInt Int
+                | SString String
+type Environment = M.Map String ServerData
+type Action = (Environment -> Body -> IO ())
+data ServerState = ServerState {
+                      serverHandlers :: [Handler]
+                    , serverEnvironment :: Environment
+                    }
+type Server = StateT ServerState IO
+
+instance Show ServerData where
+  show (SInt i) = show i
+  show (SString s) = s
+
+newServerState :: ServerState
+newServerState = ServerState [] M.empty
 
 
-addHandler :: Handler -> Server
-addHandler h = do
-  ServerState hs <- get
-  put $ ServerState (h:hs)
+addHandler :: Pattern -> Action -> Server ()
+addHandler pat act = do
+  ServerState hs env <- get
+  let h = \p -> if p == pat then Just act else Nothing
+  put $ ServerState (h:hs) env
 
 
-serve :: Server -> Request -> String
-serve srv = handle
-  where
-    handle = foldr tryHandler (const "Error - No handler found") handlers
-    tryHandler handler cont request
-      | Just s <- handler request = s
-      | otherwise = cont request
-    ServerState handlers = execState srv (ServerState [])
+setStringData :: String -> String -> Server ()
+setStringData k v = do
+  s@(ServerState _ env) <- get
+  let v'   = SString v
+      env' = M.insert k v' env
+  put $ s{ serverEnvironment=env' }
 
 
-getHandler :: Handler
-getHandler (Request t b) =
-  if t == "GET"
-  then Just b
-  else Nothing
+setIntData :: String -> Int -> Server ()
+setIntData k v = do
+  s@(ServerState _ env) <- get
+  let v'   = SInt v
+      env' = M.insert k v' env
+  put $ s{ serverEnvironment=env' }
 
 
-postHandler :: Handler
-postHandler (Request t b) =
-  if t == "POST"
-  then Just b
-  else Nothing
+handleRequest :: Request -> Server Action
+handleRequest request = do
+  ServerState registeredHandlers _ <- get
+  let action = getAction registeredHandlers request
+  return action
+    where
+      getAction = foldr tryHandler defaultHandler
+      tryHandler handler cont req
+        | Just a <- handler (requestType req) = a
+        | otherwise = cont req
+      defaultHandler = \_ _ _ ->  putStrLn "Error - No handler found"
 
 
-fooHandler :: Handler
-fooHandler (Request t b) =
-  if t == "FOO"
-  then Just b
-  else Nothing
+serverLoop :: Server ()
+serverLoop = do
+  liftIO $ putStr "Request: "
+  line <- liftIO getLine
+  let (r:bs) = words line
+      body = unwords bs
+      request = Request r body
+  unless (r == "quit") $ do
+    action <- handleRequest request
+    env <- liftM serverEnvironment get
+    liftIO $ action env body
+    serverLoop
 
 
-server :: Server
-server = do
-  addHandler getHandler
-  addHandler postHandler
-  addHandler fooHandler
+serve :: Server () -> IO ()
+serve srv = evalStateT (srv >> serverLoop) newServerState
+
+
+getAction :: Action
+getAction env body = putStrLn $ "Value: " ++ (show value) ++ "   (via GET)"
+  where value = M.findWithDefault (SString "N/A") body env
+
+
+postAction :: Action
+postAction env body = putStrLn $ "Value: " ++ (show value) ++ "   (via POST)"
+  where value = M.findWithDefault (SString "N/A") body env
+
+
+fooAction :: Action
+fooAction env body = putStrLn $ "Value: " ++ (show value) ++ "   (via FOO)"
+  where value = M.findWithDefault (SString "N/A") body env
+
+
+toyServer :: Server ()
+toyServer = do
+  setIntData "varX" 7
+  setIntData "varY" 42
+  setStringData "server_name" "Toy Server"
+  addHandler "GET" getAction
+  addHandler "POST" postAction
+  addHandler "FOO" fooAction
 
 
 main = do
   putStrLn "Starting server, enter 'quit' to stop."
-  loop
-  where
-    loop = do
-      putStr "Request: "
-      request <- getLine
-      unless (request == "quit") $ do
-        putStrLn $ serve server (Request request "body")
-        loop
+  serve toyServer

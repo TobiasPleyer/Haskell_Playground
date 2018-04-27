0343b7a9eb31067a458ace25ab978ef73b85a5ab
TobiasPleyer <tobi.pleyer@gmail.com>
2018-04-28 00:29:10 +0200

    Extract the registered moduels as UNIX paths

--- Clojure/module-scanner/src/module_scanner/core.clj
+++ Clojure/module-scanner/src/module_scanner/core.clj
@@ -1,22 +1,44 @@
 (ns module-scanner.core
   (:gen-class)
-  (:require [clojure.java.io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str])
+  (:use [clojure.java.shell :only [sh]]))
 
-(def not-start (fn [line] (not= line "IMPORTANT_DICT = {")))
-(def not-end (fn [line] (not= line "}")))
+(def not-start (partial not= "IMPORTANT_DICT = {"))
+(def not-end (partial not= "}"))
 
-(defn- get-module-lines
+(defn- get-modules
   "Extract those lines which list the module dictionary entries"
   [lines]
   (->> lines
        (drop-while not-start)
        (take-while not-end)
-       rest))
+       rest
+       (map #(str/trim
+               (first
+                 (str/split % #":"))))
+       (map #(str/replace % #"'" ""))))
+
+(defn- mk-hierarchy
+  "Splits a Python style include hierarchy string into its hierarchy
+  components.
+  Example 'path.to.package1' -> ['path' 'to' 'package1']"
+  [path-string]
+  (str/split path-string #"\."))
+
+(defn- mk-path
+  "Makes a Python inlcude path ('.' separated) to a UNIX file path
+  ('/' separated"
+  [path-string]
+  (str/replace path-string #"\." "/"))
+
 
 (defn -main
   "Make sure all of a Python package's modules are listed in a given
   configuration file"
   [& args]
-  (let [config-file (first args)]
-    (with-open [rdr (clojure.java.io/reader config-file)]
-      (doall (map println (get-module-lines (line-seq rdr)))))))
+  (let [package-dir (first args)
+        config-file (str package-dir "/config.py")
+        registered-modules (with-open [rdr (io/reader config-file)]
+                             (reduce conj [] (map mk-path (get-modules (line-seq rdr)))))]
+    (doall (for [m registered-modules] (println m)))))

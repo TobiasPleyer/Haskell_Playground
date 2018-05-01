bb463c60447f9d3e0f3292267f502929913a2c78
TobiasPleyer <tobi.pleyer@gmail.com>
2018-05-01 10:26:28 +0200

    Module scanner: bump version

--- Clojure/module-scanner/project.clj
+++ Clojure/module-scanner/project.clj
@@ -1,5 +1,5 @@
-(defproject module-scanner "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject module-scanner "1.0.0"
+  :description "Make sure a Python configuration is up-to-date"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
